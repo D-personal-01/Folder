c372e674e6be1a51ddacd0f6d8edd480d4cad511
Daksh Joshi <joshidaksh085@gmail.com>
2026-02-03 20:13:53 +0530

    More hard to comprihend the code

--- Encoding.py
+++ Encoding.py
@@ -1,7 +1,11 @@
 a=input("Enter the the messege to encode it: ")
 list(a)
 encoded=""
-for i in a:
-    s=(ord(i))**3+25*66/100
-    encoded=encoded+str(s)
+for j,i in enumerate(a):
+    if j%2==0:
+        s=ord(i)+17
+        encoded=encoded+chr(s)
+        continue
+    s=(ord(i))**3+25*66
+    encoded=encoded+str(s)+"z"
     print(encoded)
